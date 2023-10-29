461da0ce94374a2f29b2b8970c4284105377ab43
Travis Fu <travis.fu@hotmail.com>
2023-10-29 19:33:48 +0000

    i18n: add i18n locale options

---
+++ src/i18n/utils.ts
@@ -0,0 +1,11 @@
+export const localeOptions = [
+  { label: 'Deutsch', locale: 'de', key: 'de-de' },
+  { label: 'English', locale: 'en', key: 'en-us' },
+  { label: 'Español', locale: 'es', key: 'es-es' },
+  { label: 'Français', locale: 'fr', key: 'fr-fr' },
+  { label: 'Italiano', locale: 'it', key: 'it-it' },
+  { label: '日本語', locale: 'ja', key: 'ja-jp' },
+  { label: '한국어', locale: 'ko', key: 'ko-kr' },
+  { label: 'Português', locale: 'pt', key: 'pt-br' },
+  { label: '繁體中文', locale: 'zh', key: 'zh-tw' },
+];
