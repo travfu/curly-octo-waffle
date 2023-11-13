d2002fc1527d5c2ac3e7f861cf2e685a67aa909a
Travis Fu <travis.fu@hotmail.com>
2023-11-13 18:21:43 +0000

    ui: monster details basic layout

--- src/i18n/useTranslation.ts
+++ src/i18n/useTranslation.ts
@@ -7,7 +7,10 @@ import type { LocaleCode } from '@i18n/utils';
 const messages = await getCollection('messages')
 
 export const useTranslation = (locale: LocaleCode) => {
-  const getTranslation = (key: string | [string, string], selectLocale?: LocaleCode): string => {
+  const getTranslation = (key?: string | [string, string], selectLocale?: LocaleCode): string => {
+    if (!key) {
+      return ""
+    }
     if (key instanceof Array) {
       key = key.join("/")
     }
