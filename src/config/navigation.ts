86ebb8cb5061b74489cba806d2737f7496c3971d
Travis Fu <travis.fu@hotmail.com>
2023-11-18 21:13:06 +0000

    ui: add weapon list page

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -1,4 +1,5 @@
 export const NAV_CONFIG = [
   { labelKey: 'Common/MONSTER_LIST', path: 'monsters' },
+  { labelKey: 'Common/WEAPON', path: 'weapons'},
   { labelKey: 'Common/ITEM_BOX_TAB_CONSUMABLE', path: '' },
 ] as const;
