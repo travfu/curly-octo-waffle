cc410d33d85b83a51ef849354e2b512303715ff1
Travis Fu <travis.fu@hotmail.com>
2023-11-20 08:29:34 +0000

    update ElementIcon to accept element ids

--- src/constants.ts
+++ src/constants.ts
@@ -4,3 +4,16 @@ export const PartBreakType = {
     CUT: 2,
     BLUNT_ONLY: 3,
 } as const;
+
+export const ElementType = {
+    NO_ELEMENT: 0,
+    FIRE: 1,
+    WATER: 2,
+    THUNDER: 3,
+    ICE: 4,
+    DRAGON: 5,
+    POISON: 6,
+    PARALYSIS: 7,
+    SLEEP: 8,
+    BLAST: 9,
+} as const;
