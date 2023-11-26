d32426aeafd779d3122e8f68a64e0db45bdcae66
Travis Fu <travis.fu@hotmail.com>
2023-11-26 10:02:20 +0000

    chore: split out GradePillSelect

--- src/components/WeaponCard/WeaponCard.tsx
+++ src/components/WeaponCard/WeaponCard.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import type { CollectionEntry } from 'astro:content';
 import { useTranslation } from '@i18n/useTranslation';
 import { type LocaleCode } from '@i18n/utils';
-import clsx from 'clsx';
-import * as Select from '@radix-ui/react-select';
-import { CheckIcon, ChevronDownIcon } from '@radix-ui/react-icons';
 import { IconSrc } from '@utils/icon';
+import { GradePillSelect } from './GradePillSelect';
 
 type GradeAndLevelOption = {
   label: React.ReactNode;
@@ -97,62 +95,4 @@ const gradeTailwindColor = (grade: number): string => {
   else if (grade === 4) return 'bg-purple-700';
   else if (grade === 5) return 'bg-yellow-500';
   return 'bg-rose-500';
-};
-
-interface GradePillSelectProps {
-  value: string;
-  onChange: (value: string) => void;
-  options: {
-    value: string;
-    label: React.ReactNode;
-  }[];
-  placeholder?: string;
-  bgColor: string;
-}
-
-const GradePillSelect = ({
-  placeholder,
-  options,
-  value,
-  onChange,
-  bgColor,
-}: GradePillSelectProps) => {
-  return (
-    <Select.Root value={value} onValueChange={onChange}>
-      <Select.Trigger
-        className={clsx(
-          'w-[60px] flex gap-0.5 items-center justify-around rounded-md px-2 py-1 text-xs outline-none',
-          bgColor
-        )}
-      >
-        <Select.Value placeholder={placeholder} />
-        <Select.Icon>
-          <ChevronDownIcon className="w-3" />
-        </Select.Icon>
-      </Select.Trigger>
-
-      <Select.Portal>
-        <Select.Content
-          className="overflow-hidden bg-zinc-900 text-white rounded-md h-80 text-sm p-2"
-          position="popper"
-          align="center"
-        >
-          <Select.Viewport className="p-[5px]">
-            {options.map(({ value, label }) => (
-              <Select.Item
-                value={value}
-                className="pl-4 pr-2 rounded hover:bg-purple-400 outline-none flex items-center"
-              >
-                <Select.ItemText>{label}</Select.ItemText>
-                <Select.ItemIndicator className="absolute left-0 w-[25px] inline-flex items-center justify-center">
-                  <CheckIcon />
-                </Select.ItemIndicator>
-              </Select.Item>
-            ))}
-          </Select.Viewport>
-          <Select.Arrow />
-        </Select.Content>
-      </Select.Portal>
-    </Select.Root>
-  );
 };
