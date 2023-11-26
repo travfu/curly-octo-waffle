7f651c07b5aa666faaafbcedfe222959d56cdbbc
Travis Fu <travis.fu@hotmail.com>
2023-11-26 12:01:27 +0000

    chore: add bowgun ammo icons

--- src/utils/icon.ts
+++ src/utils/icon.ts
@@ -12,9 +12,26 @@ const weaponTypeTemplate = (key) => {
   return `${BASE_URL}/icons/weapon/${key}.png`
 }
 
+const bowgunAmmoTemplate = (ammoType: string) => {
+  switch (ammoType) {
+    case 'NORMAL':
+      return `${BASE_URL}/icons/spr_ui_atlas_cmn_icon_status_21.png`
+    case 'SPREAD':
+      return `${BASE_URL}/icons/spr_ui_atlas_cmn_icon_status_22.png`
+    case 'PIERCING':
+    case 'SLICING':
+      return `${BASE_URL}/icons/spr_ui_atlas_cmn_icon_status_23.png`
+    case 'STICKY':
+      return `${BASE_URL}/icons/spr_ui_atlas_cmn_icon_status_24.png`
+    default:
+      return `${BASE_URL}/icons/spr_ui_atlas_cmn_icon_status_21.png`
+  }
+}
+
 export const IconSrc = {
   ATTACK: `${BASE_URL}/icons/spr_ui_atlas_cmn_icon_status_06.png`,
   CRITICAL: `${BASE_URL}/icons/spr_ui_atlas_cmn_icon_status_02.png`,
   ELEMENT: elementTemplate,
-  WEAPON_TYPE: weaponTypeTemplate
+  WEAPON_TYPE: weaponTypeTemplate,
+  BOWGUN_AMMO: bowgunAmmoTemplate,
 };
