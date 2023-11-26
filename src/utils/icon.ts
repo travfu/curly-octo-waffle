3b6545e7d78e9886d29129f899a8131cc83430f7
Travis Fu <travis.fu@hotmail.com>
2023-11-26 09:35:38 +0000

    ui: add weapon type icon to weaponcard

--- src/utils/icon.ts
+++ src/utils/icon.ts
@@ -8,9 +8,13 @@ const elementTemplate = (key: keyof typeof ElementType) => {
   return `${BASE_URL}/icons/element/spr_ui_atlas_element_${id}.png`
 }
 
+const weaponTypeTemplate = (key) => {
+  return `${BASE_URL}/icons/weapon/${key}.png`
+}
 
 export const IconSrc = {
   ATTACK: `${BASE_URL}/icons/spr_ui_atlas_cmn_icon_status_06.png`,
   CRITICAL: `${BASE_URL}/icons/spr_ui_atlas_cmn_icon_status_02.png`,
-  ELEMENT: elementTemplate
+  ELEMENT: elementTemplate,
+  WEAPON_TYPE: weaponTypeTemplate
 };
