93ed7e5c2d2f9a9519a426b11607cc4f649e28b4
Travis Fu <travis.fu@hotmail.com>
2023-12-19 08:16:53 +0000

    astro: enable dev toolbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.mhdb.xyz',
+  devToolbar: {
+    enabled: true,
+  },
   redirects: {
     '/': '/en/monsters'
   },
